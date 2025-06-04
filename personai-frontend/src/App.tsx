5b9b13bb51280358ea34b97ccc10ba2e479c7a7e
watashiwaaniket <aniketdhakane3@gmail.com>
2025-06-05 00:07:26 +0530

    implemented routing

--- personai-frontend/src/App.tsx
+++ personai-frontend/src/App.tsx
@@ -1,11 +1,19 @@
 // import { Dashboard } from "./pages/dashboard"
+import { Dashboard } from "./pages/dashboard"
 import { Signin } from "./pages/Signin"
 import { Signup } from "./pages/Signup"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 
 function App(){
   return(
-    <Signup />
-    // <Dashboard />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
